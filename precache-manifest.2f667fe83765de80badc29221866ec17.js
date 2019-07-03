323431f27c808870c8d7c5e8f4586fa420865872
Ryan Dulac <analyticrye@gmail.com>
2019-07-03 01:05:10 -0400

    Updates

---
+++ precache-manifest.2f667fe83765de80badc29221866ec17.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7311aad4b12424f8446644904d8bbc22",
+    "url": "/Monster-Menagerie/index.html"
+  },
+  {
+    "revision": "d264e988be493ab24b89",
+    "url": "/Monster-Menagerie/static/css/main.b409affe.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/Monster-Menagerie/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "d264e988be493ab24b89",
+    "url": "/Monster-Menagerie/static/js/main.57342f21.chunk.js"
+  },
+  {
+    "revision": "227399f5b2c5c1df6293",
+    "url": "/Monster-Menagerie/static/js/runtime~main.4b049c82.js"
+  }
+]);
